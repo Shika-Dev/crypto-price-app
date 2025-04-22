f204a31b62697756fa5896a6b242db31295f4e9b
Gde Parama Artha Dharma <gededharma18@gmail.com>
2025-04-22 12:01:49 +0800

    Initial Commit

---
+++ crypto-price-app/crypto_price_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_price_appApp.swift
+//  crypto-price-app
+//
+//  Created by Parama Artha on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct crypto_price_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ crypto-price-appTests/crypto_price_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_price_appTests.swift
+//  crypto-price-appTests
+//
+//  Created by Parama Artha on 22/04/25.
+//
+
+import Testing
+@testable import crypto_price_app
+
+struct crypto_price_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
